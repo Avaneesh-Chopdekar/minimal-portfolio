6e5eb0ba9c9cae155b8b8cbd4814a11965cab217
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-08-02 11:57:59 +0530

    feat: add skill section

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 import type { Site, Metadata, Socials } from "@types";
+import type { TabPanel } from "@components/Skill.astro";
 
 export const SITE: Site = {
   NAME: "Avaneesh Chopdekar",
@@ -37,4 +38,58 @@ export const SOCIALS: Socials = [
     NAME: "twitter-x",
     HREF: "https://twitter.com/@HelloAvaneesh",
   },
+];
+
+export const skills = ["Web", "Mobile", "Backend", "Tools"];
+
+export const webItems: TabPanel[] = [
+  { name: "React.js", icon: "/icons/skills/web/react.svg" },
+  { name: "Next.js", icon: "/icons/skills/web/next.svg" },
+  { name: "Redux Toolkit", icon: "/icons/skills/web/redux-toolkit.svg" },
+  { name: "Tanstack Query", icon: "/icons/skills/web/tanstack-query.svg" },
+  { name: "Tailwind CSS", icon: "/icons/skills/web/tailwind.svg" },
+  { name: "Framer Motion", icon: "/icons/skills/web/framer-motion.svg" },
+  { name: "Jest", icon: "/icons/skills/web/jest.svg" },
+  { name: "Cypress", icon: "/icons/skills/web/cypress.svg" },
+];
+
+export const mobileItems: TabPanel[] = [
+  {
+    name: "Kotlin + XML",
+    icon: "/icons/skills/mobile/android-studio.svg",
+  },
+  {
+    name: "Jetpack Compose",
+    icon: "/icons/skills/mobile/jetpack-compose.svg",
+  },
+  { name: "React Native", icon: "/icons/skills/web/react.svg" },
+  { name: "Expo", icon: "/icons/skills/mobile/expo.svg" },
+  { name: "Flutter", icon: "/icons/skills/mobile/flutter.svg" },
+  { name: "Flutter BLOC", icon: "/icons/skills/mobile/flutter.svg" },
+  { name: "MVVM Architecture", icon: "/icons/skills/mobile/mvvm.svg" },
+];
+
+export const backendItems: TabPanel[] = [
+  { name: "Express", icon: "/icons/skills/backend/express.svg" },
+  { name: "Flask", icon: "/icons/skills/backend/flask.svg" },
+  { name: "FastAPI", icon: "/icons/skills/backend/fastapi.svg" },
+  { name: "Django", icon: "/icons/skills/backend/django.svg" },
+  { name: "MySQL", icon: "/icons/skills/backend/mysql.svg" },
+  { name: "PostgreSQL", icon: "/icons/skills/backend/postgres.svg" },
+  { name: "MongoDB", icon: "/icons/skills/backend/mongodb.svg" },
+  { name: "Redis", icon: "/icons/skills/backend/redis.svg" },
+];
+
+export const toolsItems: TabPanel[] = [
+  { name: "Visual Studio Code", icon: "/icons/skills/tools/vscode.svg" },
+  {
+    name: "Android Studio",
+    icon: "/icons/skills/mobile/android-studio.svg",
+  },
+  { name: "Git", icon: "/icons/skills/tools/git.svg" },
+  { name: "Github", icon: "/icons/skills/tools/github.svg" },
+  { name: "Docker", icon: "/icons/skills/tools/docker.svg" },
+  { name: "Gemini API", icon: "/icons/skills/tools/gemini.svg" },
+  { name: "Figma", icon: "/icons/skills/tools/figma.svg" },
+  { name: "Google Analytics", icon: "/icons/skills/tools/ga4.svg" },
 ];
